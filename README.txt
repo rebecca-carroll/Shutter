219f97593dfb26893e47997e26907ebfbd55831e
Ryan Clark <mail.w3geek@gmail.com>
2012-09-16 10:49:27 -0400

    v0.3 - Object Conversion

--- README.txt
+++ README.txt
@@ -1,10 +1,11 @@
-Shutter Image Gallery
-Shutter is an open-source JavaScript image gallery.
-Version 0.1
+Shutter Image Gallery - Shutter is an open-source JavaScript image gallery.
+Version 0.3
 
 Check the todo list below for all planned updates.
-Includes valid HTML, valid CSS, JS, and eight (8) sample images.
+Includes compliant HTML, CSS, JS and eight (8) sample images.
 Sample images belong to their rightful holders.
+
+Demo: http://w3geek.github.com/Shutter/
 
 Shutter Image Gallery is a WIP (work in progress) which means its probably riddled by bugs. If you happen to find 
 a bug or have a suggestion to make, feel free to contact me. You can use, share and modify Shutter Image Gallery as 
@@ -13,6 +14,23 @@ needed without giving credit, however, it would be appreciated.
 Contact Me by
 Email: mail.w3geek@gmail.com
 Website: http://www.thenewdev.tumblr.com/
+
+--- DOCUMENTATION ---
+
+Insert new captions & images via "captions: []" and "images: []" array located in script.js.
+The position inside the array "captions: []" and "images: []" correspond to each other.
+
+--- EXAMPLES ---
+
+captions: [
+    "Caption for photo001.jpeg",
+    "Caption for photo002.jpeg"
+]
+
+images: [
+    "photo001.jpeg",
+    "photo002.jpeg",
+]
 
 --- TODO ---
 
@@ -23,7 +41,12 @@ Website: http://www.thenewdev.tumblr.com/
 [Later] Optimize HTML, CSS and JS
 [Maybe] Add Slideshow Feature
 
+[Completed] Add Demo on "GH-PAGES" branch
+[Completed] Convert application to Objects
+[Completed] Implemented Basic Documentation
 
 --- CHANGELOG ---
+
+Version 0.3 - Added Demo, converted app to objects and implemented basic documentation.
 
 Version 0.1 - Released as Open-Source on September 9th, 2012
--- js/script.js
+++ js/script.js
@@ -1,79 +1,111 @@
-window.onload = (function () {
-    var app = "Shuttle Image Gallery",
-    caption = document.getElementById("caption"),
-    i = 0,
-    next = document.getElementById("next"),
-    path = "images/",
-    prev = document.getElementById("prev"),
-    renderer = document.getElementById("render"),
-    version = "0.1",
+/* Project: Shuttle Image Gallery                          *
+ * Developer: Ryan Clark                                   *
+ * Last Modified: September 12, 2012                       *
+ * File Name: script.js                                    *
+ * File Type: text / js                                    *
+ *                                                         */
+
+window.onload = (function() {
     
-    captions = [
-        "It looks as if Mario and Yoshi are having some fun!",
-        "A powerful Jedi from Star Wars - Knights of the Old Republic MMO.",
-        "Battlefield or Call of Duty Modern Warfare?... The world may never know.",
-        "Nathan Drake from Uncharted 3.",
-        "MasterChief, Spartan John-117 reporting for duty!",
-        "MineCraft, nuff' said!",
-        "You better watch out, Kratos is coming to town.",
-        "Max Payne 3 by Rockstar Games"
-    ],
-    
-    images = [
-        "screen01.jpeg",
-        "screen02.jpeg",
-        "screen03.jpeg",
-        "screen04.jpeg",
-        "screen05.jpeg",
-        "screen06.jpeg",
-        "screen07.jpeg",
-        "screen08.jpeg"
-    ];
-    
-    function checkScreen() {
-        if (i === 0) {
-            prev.style.visibility = "hidden";
-        } else if (i === images.length - 1) {
-            next.style.visibility = "hidden";
-        } else {
-            next.style.visibility = "visible";
-            prev.style.visibility = "visible";
-        }
-    }
-    
-    function init() {
-        checkScreen();
+    // ***** Initialization of Application ***** //
+    shutter = function() {
+        // ***** Initialization of Private Variables used by Shutter Application ***** //
+        var i = 0,
+        caption = document.getElementById("caption"),
+        next = document.getElementById("next"),
+        prev = document.getElementById("prev"),
+        renderer = document.getElementById("render");
         
-        var screen = path + images[i],
-        text = document.createTextNode(captions[i]);
+        return {
+            app: "Shutter Image Gallery",
+            author: "Ryan Clark",
+            
+            // ***** Insert Image Captions. Caption will display to corresponding image. ***** //
+            captions: [
+                       "It looks as if Mario and Yoshi are having some fun!",
+                        "A powerful Jedi from Star Wars - Knights of the Old Republic MMO.",
+                        "Battlefield or Call of Duty Modern Warfare?... The world may never know.",
+                        "Nathan Drake from Uncharted 3.",
+                        "MasterChief, Spartan John-117 reporting for duty!",
+                        "MineCraft, nuff' said!",
+                        "You better watch out, Kratos is coming to town.",
+                        "Max Payne 3 by Rockstar Games"
+            ],
+            
+            // ***** Insert Relative Paths to Images. Image will display corresponding caption. Future support for Absolute Paths. ***** //
+            images: [
+                     "screen01.jpeg",
+                     "screen02.jpeg",
+                     "screen03.jpeg",
+                     "screen04.jpeg",
+                     "screen05.jpeg",
+                     "screen06.jpeg",
+                     "screen07.jpeg",
+                     "screen08.jpeg"
+            ],
+            
+            path: "images/",
+            version: "0.3",
+            
+            // ***** Declaration of Public Methods - Do not change alter data past this point if your not a programmer. ***** //
+            checkScreen: function(real) {
+                var imgs = this.images;
         
-        caption.appendChild(text);
-        renderer.alt = captions[i];
-        renderer.src = screen;
-        
-        next.onclick = (function () { nextScreen(); return false; });
-        prev.onclick = (function () { prevScreen(); return false; });
-    }
+                if (real === 0) {
+                    prev.style.visibility = "hidden";
+                } else if (real === imgs.length - 1) {
+                    next.style.visibility = "hidden";
+                } else {
+                    next.style.visibility = "visible";
+                    prev.style.visibility = "visible";
+                }
+            },
+            
+            getVersion: function() {
+                if(this.version) {
+                    return "Version: " + this.version;
+                } else {
+                    return "Warning: Cannot read version of application!";
+                }
+            },
+            
+            init: function() {
+                this.checkScreen(i);
+                
+                var self = this,
+                screen = this.path + this.images[i],
+                text = document.createTextNode(this.captions[i]);
+                
+                caption.appendChild(text);
+                renderer.alt = this.captions[i];
+                renderer.src = screen;
+                
+                next.onclick = (function () { self.update(true); return false; });
+                prev.onclick = (function () { self.update(false); return false; });
+            },
+            
+            update: function(way) {
+                if (way === true) {
+                    i = i + 1;
+                } else if (way === false) {
+                    i = i - 1;
+                } else {
+                    return;
+                }
+                
+                this.checkScreen(i);
+                
+                var caps = this.captions[i],
+                imgs = this.images[i],
+                screen = this.path + imgs;
+                
+                caption.childNodes[0].nodeValue = caps;
+                renderer.alt = caps;
+                renderer.src = screen;
+            }
+        };
+    }();
     
-    function nextScreen() {
-        i = i + 1;
-        checkScreen();
-        
-        var screen = path + images[i];
-        caption.childNodes[0].nodeValue = captions[i];
-        renderer.alt = captions[i];
-        renderer.src = screen;
-    }
-    
-    function prevScreen() {
-        i = i - 1;
-        checkScreen();
-        
-        var screen = path + images[i];
-        caption.childNodes[0].nodeValue = captions[i];
-        renderer.alt = captions[i];
-        renderer.src = screen;
-    }
-    
-    init();
+    // ***** Call the init method on shutter object. ***** //
+    shutter.init();
 });
